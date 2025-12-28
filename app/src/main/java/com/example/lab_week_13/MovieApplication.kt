921b77f159d2a5da6ee217903a9c3f846bcaef96
Gregorius Jordan <gregoriusjordan@gmail.com>
2025-12-28 22:02:35 +0700

    Commit No. 3 – update to WorkManager

--- app/src/main/java/com/example/lab_week_13/MovieApplication.kt
+++ app/src/main/java/com/example/lab_week_13/MovieApplication.kt
@@ -1,9 +1,15 @@
 package com.example.lab_week_13
 
 import android.app.Application
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
 import com.example.lab_week_13.api.MovieService
+import com.example.lab_week_13.database.MovieDatabase
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 class MovieApplication : Application() {
     lateinit var movieRepository: MovieRepository
@@ -16,13 +22,42 @@ class MovieApplication : Application() {
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
 
+        // create a MovieDatabase instance
+        val movieDatabase =
+            MovieDatabase.getInstance(applicationContext)
+
         // create a MovieService instance
         // and bind the MovieService interface to the Retrofit instance
         // this allows us to make API calls
         val movieService = retrofit.create(
             MovieService::class.java
         )
+
+        // create a Constraints instance
+        // this will be used to specify the conditions that must be met
+        // in order to run worker tasks
+        val constraints = Constraints.Builder()
+            // only run the task if the device is connected to the internet
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        // create a WorkRequest instance
+        // this will be used to schedule a background task
+        val workRequest = PeriodicWorkRequest
+            // run the task every 1 hour
+            // even if the app is closed or the device is restarted
+            .Builder(
+                MovieWorker::class.java, 1,
+                TimeUnit.HOURS
+            ).setConstraints(constraints)
+            .addTag("movie-work").build()
+
+        // schedule the background task
+        WorkManager.getInstance(
+            applicationContext
+        ).enqueue(workRequest)
+
         // create a MovieRepository instance
-        movieRepository = MovieRepository(movieService)
+        movieRepository = MovieRepository(movieService = movieService, movieDatabase = movieDatabase)
     }
 }
--- app/src/main/java/com/example/lab_week_13/MovieRepository.kt
+++ app/src/main/java/com/example/lab_week_13/MovieRepository.kt
@@ -1,15 +1,21 @@
 package com.example.lab_week_13
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.lab_week_13.api.MovieService
+import com.example.lab_week_13.database.MovieDAO
+import com.example.lab_week_13.database.MovieDatabase
 import com.example.lab_week_13.model.Movie
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
-class MovieRepository(private val movieService: MovieService) {
+class MovieRepository(
+    private val movieService: MovieService,
+    private val movieDatabase: MovieDatabase
+) {
     private val apiKey = BuildConfig.TMDB_API_KEY
 
     // LiveData that contains a list of movies
@@ -28,9 +34,38 @@ class MovieRepository(private val movieService: MovieService) {
     // for more info, see: https://kotlinlang.org/docs/flow.html#flows
     fun fetchMovies(): Flow<List<Movie>> {
         return flow {
-            // emit the list of popular movies from the API
-            emit(movieService.getPopularMovies(apiKey).results)
-            // use Dispatchers.IO to run this coroutine on a shared pool of threads
+            // Check if there are movies saved in the database
+            val movieDao: MovieDAO = movieDatabase.movieDao()
+            val savedMovies = movieDao.getMovies()
+            // If there are no movies saved in the database,
+            // fetch the list of popular movies from the API
+            if (savedMovies.isEmpty()) {
+                val movies = movieService.getPopularMovies(apiKey).results
+                // save the list of popular movies to the database
+                movieDao.addMovies(movies)
+                // emit the list of popular movies from the API
+                emit(movies)
+            } else {
+                // If there are movies saved in the database,
+                // emit the list of saved movies from the database
+                emit(savedMovies)
+            }
         }.flowOn(Dispatchers.IO)
+    }
+
+    // fetch movies from the API and save them to the database
+    // this function is used at an interval to refresh the list of popular movies
+    suspend fun fetchMoviesFromNetwork() {
+        val movieDao: MovieDAO = movieDatabase.movieDao()
+        try {
+            val popularMovies = movieService.getPopularMovies(apiKey)
+            val moviesFetched = popularMovies.results
+            movieDao.addMovies(moviesFetched)
+        } catch (exception: Exception) {
+            Log.d(
+                "MovieRepository",
+                "An error occurred: ${exception.message}"
+            )
+        }
     }
 }
